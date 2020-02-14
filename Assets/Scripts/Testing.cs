4ba3c3f4a25c4bfb11528b471f42087c13ac49c9
Sam <wk.sams@gmail.com>
2020-02-14 16:53:07 -0300

    Grid System

---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    private Grid grid;
+
+    private void Start()
+    {
+        grid = new Grid(4, 2, 10f);
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+            grid.SetValue(Utils.GetMouseWorldPosition(), 56);
+
+        if (Input.GetMouseButtonDown(1))
+            grid.GetValue(Utils.GetMouseWorldPosition());
+    }
+}
---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utils
+{
+    public static TextMesh CreateWorldText(string text, Transform parent = null, Vector3 localPosition = default, int fontSize = 40, Color? color = null, TextAnchor textAnchor = TextAnchor.UpperLeft, TextAlignment textAlignment = TextAlignment.Left, int sortingOrder = 5000)
+    {
+        GameObject gameObject = new GameObject("World_Text", typeof(TextMesh));
+        Transform transform = gameObject.transform;
+        transform.SetParent(parent, false);
+        transform.localPosition = localPosition;
+        TextMesh textMesh = gameObject.GetComponent<TextMesh>();
+        textMesh.anchor = textAnchor;
+        textMesh.alignment = textAlignment;
+        textMesh.text = text;
+        textMesh.fontSize = fontSize;
+        textMesh.color = color != null ? (Color)color : Color.white;
+        textMesh.GetComponent<MeshRenderer>().sortingOrder = sortingOrder;
+        return textMesh;
+    }
+
+    public static Vector3 GetMouseWorldPosition()
+    {
+        Vector3 position = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+        position.z = 0f;
+        return position;
+    }
+
+    public static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera) => worldCamera.ScreenToWorldPoint(screenPosition);
+}
